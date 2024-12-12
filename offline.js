fbb5fedc8818e26f1aa8c94b00c78f5865dd681b
Dev-Ninehermits <116726139+Dev-Ninehermits@users.noreply.github.com>
2024-12-12 19:11:43 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1734010796,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite3-sheet1.png",
+		"images/sprite3-sheet2.png",
+		"images/particles.png",
+		"images/rickrolledassetsboy-sheet0.png",
+		"images/rickrolledassetsboy-sheet1.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite4-sheet1.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"media/original song.m4a",
+		"media/original song.ogg",
+		"media/rick rolled - bgm.m4a",
+		"media/rick rolled - bgm.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
